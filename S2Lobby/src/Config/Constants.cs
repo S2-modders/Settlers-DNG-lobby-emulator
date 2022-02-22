49edb49540455be212a36c3b6326138e4e8c47e1
zocker-160 <zocker1600@posteo.net>
2022-02-22 02:00:31 +0100

    add version and MOTD to constants

--- S2Lobby/src/Config/Constants.cs
+++ S2Lobby/src/Config/Constants.cs
@@ -2,15 +2,22 @@
 {
     public static class Constants
     {
+        public const uint VersionMaj = 0;
+        public const uint VersionMin = 1;
+        public static string VERSION = "v"+VersionMaj+"."+VersionMin+"-alpha 2022";
+        
+        public static string MOTD = "Welcome to The Settlers II: 10th anniversary! \n" +
+                                    " --- you are logged in as %name% --- \n\n" +
+                                    " S2 online lobby by cocomed, pnxr and zocker_160 \n" +
+                                    $"({VERSION}) \n\n" +
+                                    "Join our Discord: https://discord.gg/UAXH3VS9Qy \0";
+        
         public const string ConfigFileNameDefault = "S2Lobby.exe.config";
-
         //public const string ConfigFileName = "lobby.config";
         public const string ConfigFileName = "config.xml";
 
         public const string LoggerFileDefault = "lobby_log.txt";
-
         public const string LoggerFileDefaultDebug = "lobby_net_dump.txt";
-
         public const string LoggerTimeFormat = @"MM\/dd\/yyyy hh:mm:ss";
     }
 }
